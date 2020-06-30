a6a342e47d592171290792bb36666e1a890e43fc
Eloy Ortiz <ortizpulido.eloy@gmail.com>
2020-06-30 20:24:46 +0200

    Fin sección 3

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   anyo:number = new Date().getFullYear();
+  title:string = "SICINET";
 
   constructor() { }
 
